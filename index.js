124615a5ad660701cc68e825b953e4368b32a595
Shubham <shubhambhalodia@gmail.com>
2024-05-03 16:33:42 +0530

    made changes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var age=21;
 
 function hello(){
     // return <h1>hello guyzzz</h1>
-    return <h1>hello guyzzz</h1>
-    
+    return <>
+    <h2>hello world!</h2>
+    <h1>hello guyzzz</h1></>
+
 }
